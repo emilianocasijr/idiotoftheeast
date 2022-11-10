609d0f039f9d103d5d43b3cd355c9c65cadb0fa4
ecasi-dev <emiliano.casi@internetbrands.com>
2022-11-10 18:06:47 +0800

    Add back like changes

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Paper,
   Typography,
   CircularProgress,
   Divider,
+  Button,
 } from "@material-ui/core/";
 import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import { useParams, useHistory, Link } from "react-router-dom";
 
-import { getPost, getPostsBySearch } from "../../actions/posts";
+import { getPost, getPostsBySearch, likePost } from "../../actions/posts";
 import CommentSection from "./CommentSection";
+
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import useStyles from "./styles";
 
 const Post = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
   const { post, posts, isLoading } = useSelector((state) => state.posts);
+  const [likes, setLikes] = useState(post?.likes);
   const dispatch = useDispatch();
   const history = useHistory();
   const classes = useStyles();
@@ -35,6 +41,45 @@ const Post = () => {
   if (!post) return null;
 
   const openPost = (_id) => history.push(`/posts/${_id}`);
+
+  const userId = user?.result.googleId || user?.result?._id;
+  const hasLikedPost = post?.likes.find((like) => like === userId);
+
+  const handleLike = async () => {
+    dispatch(likePost(post._id));
+
+    if (hasLikedPost) {
+      setLikes(post?.likes.filter((id) => id !== userId));
+    } else {
+      setLikes([...post?.likes, userId]);
+    }
+  };
+
+  const Likes = () => {
+    if (likes?.length > 0) {
+      return likes.find((like) => like === userId) ? (
+        <>
+          <ThumbUpAltIcon fontSize='small' />
+          &nbsp;
+          {likes.length > 2
+            ? `You and ${likes.length - 1} others`
+            : `${likes.length} like${likes.length > 1 ? "s" : ""}`}
+        </>
+      ) : (
+        <>
+          <ThumbUpAltOutlined fontSize='small' />
+          &nbsp;{likes.length} {likes.length === 1 ? "Like" : "Likes"}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize='small' />
+        &nbsp;Like
+      </>
+    );
+  };
 
   if (isLoading) {
     return (
@@ -59,6 +104,14 @@ const Post = () => {
             alt={post.title}
           />
         </div>
+        <Button
+          size='small'
+          color='primary'
+          disabled={!user?.result}
+          onClick={handleLike}
+        >
+          <Likes />
+        </Button>
         <div className={classes.section}>
           <Typography variant='h3' component='h2'>
             {post.title}
